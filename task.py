85f8cce8720f4f72ee0093c07d542aa9090b9bc5
Sukim <kumchorong97@naver.com>
2020-04-19 02:08:27 +0900

    task 1, 2, 3

--- task.py
+++ task.py
@@ -3,8 +3,8 @@ import numpy as np
 import random
 import time
 
-MIN_NUMBER = 1e-6
-alpha = 0.0001    # Best Result : 0.01
+MIN_NUM = 1e-6
+alpha = 0.05    # Best Result : 0.01
 
 class Network:
     def __init__(self, dim, unit):
@@ -17,23 +17,23 @@ class Network:
         z = np.dot(self.w, x) + self.b # z : (unit, m)
         a = sigmoid(z) # a : (unit, m)
 
-        a = np.minimum(1 - MIN_NUMBER, a)
-        a = np.maximum(MIN_NUMBER, a) 
+        a = np.minimum(1 - MIN_NUM, a)
+        a = np.maximum(MIN_NUM, a)
 
         return a
         
     def backward(self, x, y, a, da): # y : (1, m) / a : (unit, m)
-        dz = da * derived_sigmoid(a)
-        dw = np.mean(np.dot(dz, x.T), axis=1, keepdims=True) # dw : (unit, dim)
-        db = np.mean(dz, axis = 1, keepdims=True) # db : (unit, 1)
-        
+        dz = da * derived_sigmoid(a) # dz : (unit, m)
+        dw = np.mean(np.dot(dz, x.T), axis=1, keepdims=True) # dw : (unit, 1)
+        db = np.mean(dz, axis=1, keepdims=True) # db : (unit, 1)
+
         return dw, db, np.dot(self.w.T, dz)
 
     def update(self, dw, db):
         self.w -= alpha * dw # w : (unit, dim)
         self.b -= alpha * db # b : (unit, 1)
 
-    def cost(self, y, a):
+    def loss(self, y, a):
         c = -np.mean(y * np.log(a) + (1 - y) * (np.log(1 - a)))
         return c
 
@@ -53,22 +53,22 @@ def derived_sigmoid(a):
 
 # Generate train sample shaped (dim, m)
 def generate(dim, m, rng): 
-    x = np.random.randint(rng[0], rng[1], size=(dim, m)) # (dim, m)
-    y = [] 
-    for i in np.sum(x, axis=0):
-        if i > 0:
+    x = np.random.uniform(rng[0], rng[1], size=(dim, m)) # (dim, m)
+    y = []
+    for i in range(m):
+        if x[0][i] ** 2 > x[1][i]:
             y.append(1)
         else:
             y.append(0)
     y = np.array(y) # (1, m)
     return x, y
 
-
 # Train K times
 def train(K, x, y, layers):
+    start_time = time.time()
+
     for i in range(K):
         y_hats = [x]
-
         # Forward propagation
         for layer in layers:
             y_hat = layer.forward(y_hats[-1])
@@ -84,10 +84,30 @@ def train(K, x, y, layers):
             layer.update(dw, db)
             idx += 1
 
-    # Print cost and accuracy
-    print(layers[-1].cost(y, y_hats[0]))
-    print(layers[-1].accuracy(y, y_hats[0]))
+    end_time = time.time() - start_time
 
+    # Print cost and accuracy of train set
+    #print('====================Train====================')
+    print('Loss : {}'.format(layers[-1].loss(y, y_hats[0])))
+    print('Accuracy : {}'.format(layers[-1].accuracy(y, y_hats[0])))
+    print('Execution Time : {}'.format(end_time))
+
+def test(y_hat, y, layers):
+    start_time = time.time()
+
+    for layer in layers:
+        # Forward propagation
+        y_hat = layer.forward(y_hat)
+
+    end_time = time.time() - start_time
+
+    # Print cost and accuarcy of test set
+    #print('====================Test====================')
+    print('---------------------------')
+    print('Loss : {}'.format(layers[-1].loss(y, y_hat)))
+    print('Accuracy : {}'.format(layers[-1].accuracy(y, y_hat)))
+    print('Execution Time : {}'.format(end_time))
+    #print('--------------------------------------------\n')
 
 # Create Layers
 def makeLayers(units):
@@ -102,7 +122,7 @@ dim = 2
 m = 1000
 n = 100
 data_range = [-2, 2]
-K = 1000
+K = 100
 
 # Generate train samples
 x_train, y_train = generate(dim, m, data_range)
@@ -112,18 +132,24 @@ x_test, y_test = generate(dim, n, data_range)
 task1 = makeLayers([dim, 1])
 task2 = makeLayers([dim, 1, 1])
 task3 = makeLayers([dim, 3, 1])
+#task4 = makeLayers([dim, 4, 4, 4, 1])
 
 # Train task 1
 print('========== Task1 ==========')
 train(K, x_train, y_train, task1)
-train(K, x_test, y_test, task1)
+test(x_test, y_test, task1)
 
 # Train task 2
 print('========== Task2 ==========')
 train(K, x_train, y_train, task2)
-train(K, x_test, y_test, task2)
+test(x_test, y_test, task2)
 
 # Train task 3
 print('========== Task3 ==========')
 train(K, x_train, y_train, task3)
-train(K, x_test, y_test, task3)
+test(x_test, y_test, task3)
+
+# Train task 4
+#print('========== Task4 ==========')
+#train(K, x_train, y_train, task4)
+#test(x_test, y_test, task4)
